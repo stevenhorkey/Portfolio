dcb72f4918908552f53d607979c8a280505218da
stevenhorkey <stevenhorkey@gmail.com>
2018-03-02 11:54:36 -0700

    progress

--- assets/javascript/site.js
+++ assets/javascript/site.js
@@ -2,21 +2,21 @@ var site = {
     projects : {
         'gifApp' : {
             name : "GifTastic",
-            img : 'https://www.gettyimages.ca/gi-resources/images/Homepage/Hero/UK/CMS_Creative_164657191_Kingfisher.jpg',
-            discription : 'when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishin',
-            url : 'https://www.w3schools.com/jsref/jsref_tostring_number.asp',
+            img : 'assets/images/gifapp.gif',
+            discription : 'GifTastic is a JQuery based web application which utilizes the Giphy API. The user is able to enter a topic and the amount of gifs they want generated. A button is then dynamically created and once it is clicked...Presto! The gifs are displayed. ',
+            url : 'https://stevenhorkey.github.io/GifTastic/',
         },
         'koanGame' : {
             name : "Zen Koan Game",
-            img : 'https://www.gettyimages.ca/gi-resources/images/Homepage/Hero/UK/CMS_Creative_164657191_Kingfisher.jpg',
-            discription : 'when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishin',
-            url : 'https://www.w3schools.com/jsref/jsref_tostring_number.asp',
+            img : 'assets/images/zengif.gif',
+            discription : 'What is the sound of one hand clapping? Koans are esoteric riddles that defy logical thinking and that is certainly the case in this JQuery based game. There are 15 questions, each with four possible answers. Think you get get em\' all right?',
+            url : 'https://stevenhorkey.github.io/TriviaGame/',
         },
         'lotrGame' : {
             name : "Lord of the Rings PRG",
-            img : 'https://www.gettyimages.ca/gi-resources/images/Homepage/Hero/UK/CMS_Creative_164657191_Kingfisher.jpg',
-            discription : 'when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishin',
-            url : 'https://www.w3schools.com/jsref/jsref_tostring_number.asp',
+            img : 'assets/images/test.png',
+            discription : 'Gandalf\'s dilema is a JQuery based Lord of the Rings RPG game where you play as either Smeagol, the Balrog, Sauron, or Gandalf himself. Each time you attack, you also endure some damage, but your power grows evermore. You may win, you may loose, or you all may die in the process. Which will it be?',
+            url : 'https://stevenhorkey.github.io/week-4-game/',
         },
         // 'mmSite' : {
         //     name : ,
@@ -42,21 +42,21 @@ var site = {
             var project = $('<div>');
             project.addClass(pLabel.name.replace(/\s+/g, '-').toLowerCase());
             var image = $('<img>').attr("src", pLabel.img);
-            image.addClass('col-md-6');
+            image.addClass('col-md-6 project-image');
             var text = $('<div>').addClass('project-text col-md-6');
             var title = $('<h2>').addClass('project-title');
             title.text(pLabel.name);
             var description = $('<p>').addClass('project-description');
-            description.text(pLabel.description);
+            description.text(pLabel.discription);
             var checkUrl = $('<a>').attr('href',pLabel.url).attr('target','_blank');
             checkUrl.addClass('project-url').text("Check it out here")
             text.append(title).append(description).append(checkUrl);
             if (imgLayout === 'right'){
                 project.append(text).append(image); 
-                text.css('float','left') ;
+                text.css('float','left');
             } else if (imgLayout === 'left') {
                 project.append(image).append(text);
-                text.css('float','right') ;                
+                text.css('float','right');                
             } else {
                 alert('error with project build')
             }
@@ -68,4 +68,6 @@ var site = {
 
 $(document).ready(function(){
     site.functions.buildProject(site.projects.gifApp,'left','.proj-1')
+    site.functions.buildProject(site.projects.koanGame,'right','.proj-2')
+    site.functions.buildProject(site.projects.lotrGame,'left','.proj-3')
 });
